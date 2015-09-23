b883ba3abd7d04b8f8ad7342ce85908ee534c442
Pedro Torres Picón <pedro@pedrotp.com>
2015-09-23 05:22:56 +0000

    added a preview method to show shorter note snippets

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,9 +1,14 @@
 class Note < ActiveRecord::Base
+  
   belongs_to :user
 
   validates :title, presence: true
   validates :content, presence: true
 
   scope :sorted, lambda { order('created_at DESC') }
+  
+  def preview
+    self.content.slice(0..100) << "...(readmore)"
+  end
 
 end
